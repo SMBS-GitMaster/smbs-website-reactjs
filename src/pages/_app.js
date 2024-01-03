7dffb05c64ab96a363a873a710835a957663b36b
kenley.gaitan <kenley.gaitan@smbssolutions.com>
2024-01-03 15:34:06 -0600

    it was created the code structure

---
+++ src/pages/_app.js
@@ -0,0 +1,9 @@
+import "@/styles/css/main.css";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
+}
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function HomePage() {
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+        <meta name="description" content="home" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+    </>
+  );
+}
