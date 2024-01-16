c8cb60ad4e173799c9de7ef3e58144d57863ed3d
kenley.gaitan <kenley.gaitan@smbssolutions.com>
2024-01-16 16:43:04 -0600

    fix: it was resolved the conflicts

--- src/components/HomeHero/index.js
+++ src/components/HomeHero/index.js
@@ -1,5 +1,3 @@
-import Image from "next/image";
-import Button from "../Button";
 import Hero from "../Hero";
 import CardStack from "../CardStack";
 import Carousel from "react-multi-carousel";
@@ -23,34 +21,17 @@ const HomeHero = () => {
     ));
 
   return (
-    <Hero>
-      <div className="hero__description">
-        <h2 className="title hero__title">
-          Boost your Software team with LATAM talent.
-        </h2>
-        <h3 className="title title--primary hero__title">
-          Staff augmentation made easy.
-        </h3>
-        <p className="text hero__text">
-          Whether you need one developer or an entire team, we have the
-          expertise to find the right fit for your project. Plus, with our
-          flexible engagement models, you can choose the level of support that
-          works best for your needs.
-        </p>
-        <div className="hero__content__bottom">
-          <Button
-            customClass="button hero__content__button"
-            buttonText="Get in Touch"
-          />
-          <Image
-            src="/image_2.png"
-            width={150}
-            height={162}
-            alt="Clutch logo"
-          />
-        </div>
-      </div>
-
+    <Hero
+      title="Boost your Software team with LATAM talent."
+      subtitle="Staff augmentation made easy."
+      text="Whether you need one developer or an entire team, we have the
+      expertise to find the right fit for your project. Plus, with our
+      flexible engagement models, you can choose the level of support that
+      works best for your needs."
+      buttonText="Get in Touch"
+      isShowLogo
+      logo="/image_2.png"
+    >
       <div className="hero__stack">
         <Carousel
           autoPlay
